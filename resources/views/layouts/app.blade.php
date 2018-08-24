70112ddc9860b76d528a56f665c9da0cb7170aa8
rb5169 <robert.blonski@hurra.com>
2018-08-24 16:01:38 +0200

    hide menu part for not logged in users

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,13 +23,15 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-            <div class="container">
+            <div class="container-fluid">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
+                @auth
                 <a class="navbar-brand" href="{{ url('/strava') }}">
                     Strava
                 </a>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
